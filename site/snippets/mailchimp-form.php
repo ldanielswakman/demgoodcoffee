af2e94310625873deeeddda175fc7921f3105ce8
ldanielswakman <d.swakman@gmail.com>
2019-09-06 12:57:20 +0200

    Added mailchimp form URL

--- site/snippets/mailchimp-form.php
+++ site/snippets/mailchimp-form.php
@@ -1,4 +1,4 @@
-<? $target_url = '' ?>
+<? $target_url = isset($target_url) ? $target_url : ''; ?>
 
 <!-- Begin Mailchimp Signup Form -->
 <div id="mc_embed_signup">
@@ -23,7 +23,7 @@
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7af2dded214d98b31ad653a10_204af0ef61" tabindex="-1" value=""></div>
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_33cbb56428447c6f07f1e8e80_348fda02bb" tabindex="-1" value=""></div>
     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
 </form>
--- site/tags/form.php
+++ site/tags/form.php
@@ -2,11 +2,12 @@
 
 kirbytext::$tags['form'] = array(
   'attr' => array(
+    'url'
   ),
   'html' => function($tag) {
 
     if($tag->attr('form') == 'mailchimp') {
-      $html = snippet('mailchimp-form', [], true);
+      $html = snippet('mailchimp-form', ['target_url' => $tag->attr('url')], true);
     } else {
       $html = 'No form found...';
     }
