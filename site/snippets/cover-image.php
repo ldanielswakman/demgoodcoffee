681bd503a7dceb6f76635df5f526d91a4be6eaa4
ldanielswakman <d.swakman@gmail.com>
2019-09-06 12:51:42 +0200

    Cover image update

--- site/snippets/cover-image.php
+++ site/snippets/cover-image.php
@@ -1,10 +1,15 @@
-<? $p = isset($p) ? $p : $page ?>
-<? if($p->coverimage()->isNotEmpty()): ?>
+<?php
+$p = isset($p) ? $p : $page;
+$cover = isset($cover) ? $cover : false;
+if($p->coverimage_fit()->isNotEmpty() && $p->coverimage_fit() == 'cover') { $cover = true; }
+?>
 
-	<? $img = $p->image($p->coverimage()) ?>
-	<figure class="cover-image">
+<?php if($p->coverimage()->isNotEmpty()): ?>
+
+	<?php $img = $p->image($p->coverimage()) ?>
+	<figure class="cover-image<?= ($cover == true) ? ' cover-image--full' : '' ?>">
 		<div class="bg" style="background-image: url('<?= $img->thumb(['width' => 1200])->url() ?>')"></div>
 		<img src="<?= $img->thumb(['width' => 1200])->url() ?>" alt="<?= $img->alt() ?>" title="<?= $img->alt() ?>" />
 	</figure>
 	
-<? endif ?>
+<?php endif ?>
