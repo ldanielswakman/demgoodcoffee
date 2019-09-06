0137b811c1e2b087b2950680fb186be525cba879
ldanielswakman <d.swakman@gmail.com>
2019-09-06 12:52:35 +0200

    Updated help panel

--- site/widgets/help-docs/help-docs.html.php
+++ site/widgets/help-docs/help-docs.html.php
@@ -3,7 +3,24 @@
 <i>Some useful links when you're working on content:</i>
 <br><br>
 <a href="http://commonmark.org/help/" class="btn btn-rounded" target="_blank"><i class="fa fa-info-circle"></i> &nbsp; Markdown help</a>
+<br />
+<br />
+Addition: if you want to set a text in one of the 2 <span style="color: #E95C0F;">theme</span> <span style="color: #2DB8BD;">colours</span>, use:<br />
+<pre class="example-code">&lt;span class="orange"&gt;<span style="color: #E95C0F;">[text]</span>&lt;/span&gt;</pre>
+<pre class="example-code">&lt;span class="teal"&gt;<span style="color: #2DB8BD;">[text]</span>&lt;/span&gt;</pre>
 <br><br>
 <a href="options" class="btn btn-rounded"><i class="fa fa-cog"></i> &nbsp; Site options</a>&nbsp;&nbsp;&nbsp;
 <br /><br />
 <a href="translations" class="btn btn-rounded"><i class="fa fa-language"></i> &nbsp; Translations</a>
+
+<style>
+	.example-code {
+		 display: inline-block;
+		 margin-top: 0.25rem;
+		 padding: 0.25rem;
+		 background: #f5f6f7; 
+		 border-radius: 0.125rem;
+		 font-family: monospace;
+		 color: #666;
+	}
+</style>
